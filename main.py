01f86035cb809bed21f1c2811d62f5d661a74838
ItaloSSilva19 <italo.s.silva@rede.ulbra.br>
2022-08-24 00:00:07 -0300

    alterando tudo

--- main.py
+++ main.py
@@ -1,186 +1,173 @@
-from ast import IsNot
-from operator import contains, is_not
 from random import randint
+from collections import Counter
+
 
 # lista composta por: Turma, Professor, Quantidade de Alunos e Tipo de local (Labin ou Sala de aula)
 eventos = [
-    ['A1', 1, 27, 1],
-    ['A2', 2, 33, 1],
-    ['A3', 4, 25, 1],
-    ['A4', 4, 25, 0],
-    ['A5', 5, 42, 0],
-    ['B1', 1, 31, 1],
-    ['B2', 2, 26, 1],
-    ['B3', 3, 20, 1],
-    ['B4', 5, 45, 0],
-    ['B5', 6, 42, 0],
-    ['C1', 6, 20, 1],
-    ['C2', 5, 19, 1],
-    ['C3', 1, 43, 1],
-    ['C4', 6, 45, 1],
-    ['D1', 7, 30, 1],
-    ['D2', 2, 20, 1],
-    ['D3', 7, 30, 1],
-    ['D4', 3, 20, 1],
-    ['E1', 7, 27, 0],
-    ['E2', 2, 22, 1],
-    ['E3', 7, 26, 1],
-    ['E4', 4, 25, 1],
-    ['E5', 7, 25, 1],
-    ['F1', 4, 22, 1],
-    ['F2', 6, 30, 1],
-    ['F3', 1, 21, 1],
-    ['F4', 3, 25, 0],
-    ['G1', 3, 25, 1],
-    ['G2', 4, 44, 1],
-    ['G3', 5, 44, 0],
-    ['H1', 1, 25, 1],
-    ['H2', 3, 44, 1],
-    ['H3', 5, 44, 0]
+    ["A1", 1, 27, 1],
+    ["A2", 2, 33, 1],
+    ["A3", 4, 25, 1],
+    ["A4", 4, 25, 0],
+    ["A5", 5, 42, 0],
+    ["B1", 1, 31, 1],
+    ["B2", 2, 26, 1],
+    ["B3", 3, 20, 1],
+    ["B4", 5, 45, 0],
+    ["B5", 6, 42, 0],
+    ["C1", 6, 20, 1],
+    ["C2", 5, 19, 1],
+    ["C3", 1, 43, 1],
+    ["C4", 6, 45, 1],
+    ["D1", 7, 30, 1],
+    ["D2", 2, 20, 1],
+    ["D3", 7, 30, 1],
+    ["D4", 3, 20, 1],
+    ["E1", 7, 27, 0],
+    ["E2", 2, 22, 1],
+    ["E3", 7, 26, 1],
+    ["E4", 4, 25, 1],
+    ["E5", 7, 25, 1],
+    ["F1", 4, 22, 1],
+    ["F2", 6, 30, 1],
+    ["F3", 1, 21, 1],
+    ["F4", 3, 25, 0],
+    ["G1", 3, 25, 1],
+    ["G2", 4, 44, 1],
+    ["G3", 5, 44, 0],
+    ["H1", 1, 25, 1],
+    ["H2", 3, 44, 1],
+    ["H3", 5, 44, 0],
 ]
+horarios = ["segunda", "terça", "quarta", "quinta", "sexta"]
+
 locais = [
-    ['L1', 50],
-    ['L2', 0],
-    ['L3', 20],
-    ['L4', 30],
-    ['L5', 40],
-    ['L6', 40],
-    ['S1', 50],
-    ['S2', 50],
-    ['S3', 40]
+    ["L1", 50],
+    ["L2", 0],
+    ["L3", 20],
+    ["L4", 30],
+    ["L5", 40],
+    ["L6", 40],
+    ["S1", 50],
+    ["S2", 50],
+    ["S3", 40],
 ]
 
-dias_semana = ['segunda', 'terça', 'quarta', 'quinta', 'sexta']
 
-tamanho_populacao = 33*7*50*4*9
-
-
-def gerador_cromossomo():
-    '''
-        gerador de cromossomo, estrutura é a seguinte:
-        |Disciplina(0-32)|Professor(0-6)|Alunos(0-50)|Tipo(0-1)|Dia(0-4)|Local(0-8)|Aptidao = 0
-
-        essa função retorna essa estrutura como uma lista.
-    '''
-    return [randint(0, 32), randint(0, 6), randint(0, 50), randint(0, 1), randint(0, 4), randint(0, 8), 0]
+def gerar_gene():
+    """Gera a estrutura usada em cada gene.
+    A estrutura é a seguinte:
+        |Disciplina(0-32)|Professor(0-6)|Alunos(0-50)|Tipo(0-1)|Dia(0-4)|Local(0-8)|Aptidao = 0|
+    """
+    return [randint(0, 32), randint(0, 6), randint(0, 50), randint(0, 1)]
 
 
-def gerar_populacao_inicial():
-    '''
-        população inicial será composta por 100 cromossomos
-    '''
-    lista_populacao_inicial = []
-    for i in range(tamanho_populacao):
-        lista_populacao_inicial.append(gerador_cromossomo())
-    return lista_populacao_inicial
+def gerar_cromossomo():
+    """
+    Estrutura do cromossomo é um vetor (HxL) Horários, Locais.
+    Cada gene do cromossomo é corresponde a um evento.
+    """
+    cromossomo = []
+    horarios = 5
+    locais = 9
+
+    for i in range(horarios*locais):
+        cromossomo.append(gerar_gene())
+
+    return cromossomo
+
+
+#print('Estrutura cromossomo',gerar_cromossomo(),'\n---------------')
+
+
+def gerar_populacao():
+    """
+    Gera a população inicial.
+    O tamanho dela é dado pelo produto entre Horários, Locais e Eventos.
+    """
+    #tamanho_populacao = 5 * 9 * 32
+    tamanho_populacao = 2
+    populacao_inicial = []
+    for i in range(0, tamanho_populacao):
+        populacao_inicial.append(gerar_cromossomo())
+
+    return populacao_inicial
 
 
 def calcular_aptidao(cromossomo):
-    cromossomo_aux = cromossomo.copy()
-    cromossomo_aux[0] = eventos[cromossomo[0]][0]
-    if cromossomo_aux[0:4] in eventos:
-        return True
-    else:
-        return False
+    restricoes_duras = 100
+    restricoes_leves = 15
+    lista_horarios_professor = []
+    penalidade_colisao_professor = 0
+    penalidade_colisao_turma = 0
 
-def dizimacao(populacao):
-    horarios_indisponiveis = []
-    turmas_indisponiveis = []
-    populacao_dizimada = []
-    for cromossomo in populacao:
-        horario = cromossomo[4:6]
-        turma = cromossomo[0]
-        if turma not in turmas_indisponiveis:
-            turmas_indisponiveis.append(turma)
-            #verifica se tem o horario e se não é no labin 2
-            if horario not in horarios_indisponiveis and cromossomo[5] != 1:
-                horarios_indisponiveis.append(horario)
-                populacao_dizimada.append(cromossomo)
-    
+    lista_horarios_turma = []
 
-    return populacao_dizimada
-
-
-def crossover(populacao):
-    tamanho_populacao = len(populacao)
-    populacao_cruzada= []
-
-    if tamanho_populacao % 2 == 0:
-        for i in range(1,tamanho_populacao,2):
-            primeiro_filho = []
-            segundo_filho = []
-            primeira_parte_pai_1 = populacao[i-1][:4]
-            segunda_parte_pai_1 = populacao[i-1][-3:]
-            primeira_parte_pai_2 = populacao[i][:4]
-            segunda_parte_pai_2 = populacao[i][-3:]
-            
-            primeiro_filho.extend(primeira_parte_pai_1)
-            primeiro_filho.extend(segunda_parte_pai_2)
-            segundo_filho.extend(primeira_parte_pai_2)
-            segundo_filho.extend(segunda_parte_pai_1)
-            
-            populacao_cruzada.append(primeiro_filho)
-            populacao_cruzada.append(segundo_filho)
-    else:
-        for i in range(1, (tamanho_populacao),2):
-            primeiro_filho = []
-            segundo_filho = []
-            primeira_parte_pai_1 = populacao[i-1][:4]
-            segunda_parte_pai_1 = populacao[i-1][-3:]
-            primeira_parte_pai_2 = populacao[i][:4]
-            segunda_parte_pai_2 = populacao[i][-3:]
-            
-            primeiro_filho.extend(primeira_parte_pai_1)
-            primeiro_filho.extend(segunda_parte_pai_2)
-            segundo_filho.extend(primeira_parte_pai_2)
-            segundo_filho.extend(segunda_parte_pai_1)
-            
-            populacao_cruzada.append(primeiro_filho)
-            populacao_cruzada.append(segundo_filho)
-        
-        populacao_cruzada.append(populacao[-1])        
-
-    return populacao_cruzada
-
-def multacao_gene(populacao):
-    for cromossomo in populacao:
-        aleatoriedade = randint(0,100)
-        if aleatoriedade < 10:
-            cromossomo[4] = randint(0,4)
-        elif aleatoriedade >=10 and aleatoriedade < 20:
-            cromossomo[5] = randint(0,8)
+    for i in range(len(cromossomo)):
+        professor = eventos[cromossomo[i][0]][1]
+        turma = eventos[cromossomo[i][0]][0]
+        if i <= 8:
+            lista_horarios_professor.append(str(professor) + 'seg')
+            lista_horarios_turma.append(str(turma) + 'seg')
+        elif i <= 17:
+            lista_horarios_professor.append(str(professor) + ' ter')
+            lista_horarios_turma.append(str(turma) + ' ter')
+        elif i <= 26:
+            lista_horarios_professor.append(str(professor) + ' qua')
+            lista_horarios_turma.append(str(turma) + ' qua')
+        elif i <= 35:
+            lista_horarios_professor.append(str(professor) + 'qui')
+            lista_horarios_turma.append(str(turma) + 'qui')
+        elif i <= 44:
+            lista_horarios_professor.append(str(professor) + 'sex')
+            lista_horarios_turma.append(str(turma) + 'sex')
         else:
-            cromossomo[0] = randint(0,32)
+            break
+    colisoes_professores = Counter(lista_horarios_professor).values()
+    colisoes_turmas = Counter(lista_horarios_turma).values()
+
+    for elemento in colisoes_professores:
+        if elemento > 1:
+            penalidade_colisao_professor += elemento * restricoes_duras
+
+    for elemento in colisoes_turmas:
+        if elemento > 1:
+            penalidade_colisao_turma += elemento * restricoes_duras
+
+    print(penalidade_colisao_professor)
+
+    return cromossomo
+
+
+cromo = [[15, 3, 22, 1], [15, 1, 35, 0], [26, 4, 35, 0], [20, 5, 33, 1], [14, 6, 24, 0], [15, 1, 36, 0], [26, 2, 46, 1], [20, 5, 20, 1], [10, 1, 31, 1], [27, 4, 32, 0], [9, 6, 14, 0], [2, 2, 5, 1], [21, 2, 25, 1], [27, 5, 14, 1], [19, 3, 37, 1], [10, 5, 13, 0], [11, 2, 25, 1], [5, 3, 15, 0], [22, 1, 42, 1], [32, 4, 34, 0], [13, 5, 30, 0], [19, 5, 39, 0], [15, 6, 24, 1], [7, 4, 49, 0], [15, 0, 44, 0], [15, 5, 40, 0], [7, 3, 15, 0], [9, 5, 14, 0], [32, 4, 5, 0], [31, 5, 18, 0], [7, 6, 37, 0], [30, 0, 6, 1], [28, 5, 39, 1], [29, 4, 45, 0], [15, 0, 18, 1], [15, 3, 48, 0], [30, 4, 9, 0], [27, 6, 37, 0], [17, 2, 50, 1], [23, 3, 35, 1], [11, 6, 17, 0], [28,
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               5, 49, 1], [19, 2, 11, 1], [3, 5, 47, 0], [12, 2, 12, 1]]
+
+calcular_aptidao(cromo)
+
+
+def selecao_torneio(populacao):
+    pass
+
+
+def crossover_ponto_unico(primeiro_pai, segundo_pai):
+    pass
+
+
+def multacao_com_mascara(cromossomo):
+    pass
+
+
+def executar():
+    populacao = gerar_populacao()
+    t = 0
+    lista_schedule_viaveis = []
+
+    while t < 1:
+        for cromossomo in populacao:
+            calcular_aptidao(cromossomo)
+
+        t += 1
     return populacao
 
-def encontrar_solucao():
-    populacao_inicial = gerar_populacao_inicial()
-    populacao_compativel = []
-    t = 0
-    nao_solucionou_problema = True
 
-    print(f'Geração: {t} \nPopulação: {len(populacao_inicial)}')
-
-
-    for cromossomo in populacao_inicial:
-        if calcular_aptidao(cromossomo):
-            cromossomo[-1] = cromossomo[-1] +1
-            populacao_compativel.append(cromossomo)
-    
-
-    while nao_solucionou_problema:
-        t += 1
-        proxima_geracao = dizimacao(populacao_compativel)
-        
-        if len(proxima_geracao) < 35:
-            print(f'Geração: {t} \nPopulação: {len(proxima_geracao)}') 
-            proxima_geracao_aux = crossover(populacao_inicial)
-            proxima_geracao_aux = multacao_gene(populacao_inicial)
-            populacao_compativel = proxima_geracao_aux
-        else:
-            print(f'Geração: {t} \Solução: {proxima_geracao}') 
-            nao_solucionou_problema = False
-            populacao_compativel.extend(proxima_geracao)
-        
-
-encontrar_solucao()
+# print(len(executar()))
+executar()
